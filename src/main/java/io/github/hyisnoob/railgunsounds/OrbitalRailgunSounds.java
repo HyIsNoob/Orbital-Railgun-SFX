2bca4335a9798d81176407a9bb5f82c17c79b8fe
Julian Speckmann <176780813+KingIronMan2011@users.noreply.github.com>
2025-10-17 05:16:46 +0200

    Refactor logger implementation to use java.util.logging instead of SLF4J

--- src/main/java/io/github/hyisnoob/railgunsounds/OrbitalRailgunSounds.java
+++ src/main/java/io/github/hyisnoob/railgunsounds/OrbitalRailgunSounds.java
@@ -1,16 +1,16 @@
 package io.github.hyisnoob.railgunsounds;
 
+import java.util.logging.Logger;
+
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class OrbitalRailgunSounds implements ModInitializer {
     public static final String MOD_ID = "orbital_railgun_sounds";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = Logger.getLogger(MOD_ID);
 
     public static final Identifier RAILGUN_SHOOT_ID = new Identifier(MOD_ID, "railgun_shoot");
     public static final Identifier SCOPE_ON_ID = new Identifier(MOD_ID, "scope_on");
